a038eebbde3a972e5b6eb09f0fb17bb637ced09e
srodenas <srodher115@g.educaand.es>
2023-09-01 20:39:27 +0200

    Actualizo para que permita recoger la raza mediante un botón de búsqueda. Observamos un cambio en dicho campo, la cual modificaremos la lista que automaticamente se observará un nuevo cambio en el viewmodel de dogListLiveData. Volverá a cargar los datos. También hacemos para que se cierre el teclado.

--- app/src/main/java/com/pmdm/virgen/dogapi/ui/modelview/DogViewModel.kt
+++ app/src/main/java/com/pmdm/virgen/dogapi/ui/modelview/DogViewModel.kt
@@ -10,6 +10,7 @@ class DogViewModel : ViewModel() {
     var dogListLiveData = MutableLiveData<List<String>>()  //Creamos la lista a observar.
     var progressBarLiveData = MutableLiveData<Boolean>()  //para la barra de progreso.
     val getDogsRepositoryUseCase = GetDogsRepositoryUseCase()  //Creo el repositorio
+    var search = MutableLiveData<String>() //para el campo search
 
 
 
@@ -18,6 +19,24 @@ class DogViewModel : ViewModel() {
         viewModelScope.launch {
             progressBarLiveData.postValue(true)  //se ve.
             getDogsRepositoryUseCase.initRepository("maltese") //esto es superfeo, pero para ver como funciona.
+            val result: List<String>? = getDogsRepositoryUseCase()
+            result.let { myList ->
+                dogListLiveData.postValue(myList)  //notificamos el cambio y el observer se enterará
+                progressBarLiveData.postValue(false)
+            }
+        }
+    }
+
+
+
+    public fun searchByBreed(breed: String){
+        search.postValue(breed)
+    }
+
+    public fun searchByBreedList(breed: String){
+        viewModelScope.launch {
+            progressBarLiveData.postValue(true)
+            getDogsRepositoryUseCase.initRepository(breed) //esto es superfeo, pero para ver como funciona.
             val result: List<String>? = getDogsRepositoryUseCase()
             result.let { myList ->
                 dogListLiveData.postValue(myList)  //notificamos el cambio y el observer se enterará
--- app/src/main/java/com/pmdm/virgen/dogapi/ui/view/MainActivity.kt
+++ app/src/main/java/com/pmdm/virgen/dogapi/ui/view/MainActivity.kt
@@ -2,7 +2,9 @@ package com.pmdm.virgen.dogapi.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,11 +14,12 @@ import com.pmdm.virgen.dogapi.ui.adapter.DogAdapter
 import com.pmdm.virgen.dogapi.ui.modelview.DogViewModel
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.mySearch.setOnQueryTextListener(this)
         registerLiveData()
         init()  //inicializa la clase
        // testApiDog3()  //para el testeo de retrofit
@@ -29,7 +32,9 @@ class MainActivity : AppCompatActivity() {
     private val dogViewModel : DogViewModel by viewModels()
 
 
-
+    /*
+    Supuestamente, se llamará cuando cambie la lista de datos.
+     */
     private fun registerLiveData() {
         dogViewModel.dogListLiveData.observe(
             this, {  myList->
@@ -45,6 +50,30 @@ class MainActivity : AppCompatActivity() {
                 binding.progressBar.isVisible = visible
             }
         )
+
+        //observo cualquier cambio en el componente search
+        dogViewModel.search.observe(
+            this, {  breed->
+                searchByBreed(breed)  //ordeno cargar los datos.
+            }
+        )
+    }
+
+
+    /*
+    método que hace que se carguen los nuevos datos por raza y se modificará
+    el observable dogListLiveData
+     */
+    private fun searchByBreed(breed: String?) {
+        dogViewModel.searchByBreedList(breed!!)
+        hideKeyBoard()
+    }
+
+
+
+    private fun hideKeyBoard() {
+        val imn = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imn.hideSoftInputFromWindow(binding.myLayoutPpal.windowToken, 0)
     }
 
 
@@ -70,13 +99,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-
-
-
-
-
     //Test de pruebas de que funciona la petición a Retrofit
     private fun testApiDog3() {
         lifecycleScope.launch {
@@ -84,6 +106,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    /*
+    Si el texto que escribimos en el campo my_search no es nulo y vacío, tenemos que llamar al método que
+    nos cargue todas las imágenes por texto introducido.
+     */
+    override fun onQueryTextSubmit(query: String?): Boolean {
+
+        if (!query.isNullOrEmpty())
+            dogViewModel.searchByBreed(query!!)
+        return true
+    }
+
+
+    /*
+    Cualquier cambio, llamará a este método. Estoy obligado a ponerlo
+     */
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
 
 
 }
